1933604a49a93673d21f67743e1a0bb568363522
Christian Dietrich <stettberger@dokucode.de>
2010-07-17 12:28:23 +0200

    added help command

--- gmp.py
+++ gmp.py
@@ -188,6 +188,8 @@ at <local_directory>/<github_project_name>"""
 class RepoManager:
     """Manages all repositories and provides the command line interface"""
     sets = {}
+    help_commands = {"selector": """A selector is a regexp which is checked against
+<sets>:<clone-url> of a repository. So '^<set>:' will only select repositories within a given set."""}
 
     def __init__(self):
         self.hostname = getfqdn()
@@ -198,9 +200,8 @@ class RepoManager:
                          "push" : self.shortcut("push"),
                          "pull" : self.shortcut("pull"),
                          "fetch" : self.shortcut("fetch"),
-                         "sets"  : self.cmd_sets}
-
-    
+                         "sets"  : self.cmd_sets,
+                         "help" : self.cmd_help}
 
     def __call__(self):
         """The Reposity Manager can be called in order to start the command
@@ -239,6 +240,12 @@ line interface"""
         return repos
     
     def cmd_list(self, selector):
+        """Lists all repositories, which matches the selector. If no selector given 
+list all repositories. See help selector for help with selectors.
+
+metagit list == metagit list all
+
+usage: metagit list <selector>"""
         if len(selector) == 0:
             selector = ["all"]
         repos = self._select(selector[0])
@@ -266,10 +273,18 @@ line interface"""
             return ["all"]
         return args
 
-    def shortcut(self, git_command):
-        return lambda x: self.cmd_foreach([self._shortcut(x)[0], git_command] + self._shortcut(x)[1:])
+    def shortcut(self, git_command, help = None):
+        if not help:
+            help = "alias %s <selector> = foreach <selector> %s" %(git_command, git_command)
+        func = lambda x: self.cmd_foreach([self._shortcut(x)[0], git_command] + self._shortcut(x)[1:])
+        func.__doc__ = help
+        return func
 
     def cmd_foreach(self, args):
+        """usage: metagit foreach <selector> <git-command>
+ executes git command on all repositories matching the selector
+
+help selector for more information on selectors"""
         if len(args) < 2:
             self.die("Not enough arguments")
         repos = self._select(args[0])
@@ -283,6 +298,7 @@ line interface"""
             a.wait()
 
     def cmd_sets (self, args):
+        """Show only git repository sets"""
         if len(args) < 1:
             args = [".*all"]
         else:
@@ -293,3 +309,20 @@ line interface"""
                 print "%s:" % key
                 for repo in self.sets[key]:
                     print "  " + repo.clone_url + " --> " + repo.local_url
+
+    def cmd_help(self, args):
+        """recursive: see recursive"""
+        if len(args) < 1:
+            self.die("No topic selected")
+        if args[0] in self.commands:
+            doc = self.commands[args[0]].__doc__
+            if doc:
+                print doc
+            else:
+                self.die("No help available")
+
+        elif args[0] in self.help_commands:
+            print self.help_commands[args[0]]
+
+        else:
+            self.die("No help available")
