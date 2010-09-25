7f858a0b6f576b6881992b7070bd5497eca0af86
Christian Dietrich <stettberger@dokucode.de>
2010-09-25 23:39:20 +0200

    added version detection

--- setup.py
+++ setup.py
@@ -2,10 +2,19 @@
 
 from distutils.core import setup
 
+# The version is the latest git tag
+import subprocess
+a = subprocess.Popen("git tag | grep '^v' | tail -n 1", shell = True, stdout = subprocess.PIPE)
+version = a.stdout.readline().strip().replace("v", "")
+a.wait()
+
 setup(
     name='metagit',
-    version=r'',
+    version= version,
+	author = "Christian Dietrich",
+	author_email = "stettberger@dokucode.de",
     description='managing big amount of git repositories',
+	license = "GPLv3",
     url='http://github.com/stettberger/metagit/',
     packages=['gmp'],
     scripts=['metagit']
